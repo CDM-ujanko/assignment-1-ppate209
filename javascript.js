1cb67bca76d7afa59b80f4f7e9ebc8352f6bb24e
ppate209 <49217064+ppate209@users.noreply.github.com>
2019-04-07 21:01:19 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log('The new file');
+console.info('This is extra credit');
+var x = 'used console.info';
+console.log (x);
+
+console.log(document);
+console.log($(document));
+console.log($('div'));
+
+console.log($); 
+$(document).ready(function(){
+     
+   console.log('Ready');
+}); 
